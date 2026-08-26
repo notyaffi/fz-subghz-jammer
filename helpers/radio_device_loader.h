06a2122f40857aadf712be79bffce4308c271b88
yaffi <162258437+notyaffi@users.noreply.github.com>
2026-08-26 17:47:49 -0400

    Added explicit loader APIs

--- helpers/radio_device_loader.h
+++ helpers/radio_device_loader.h
@@ -4,9 +4,11 @@
 
 typedef enum {
     RadioDeviceLoaderStatusOk,
-    RadioDeviceLoaderStatusNotFound,
-    RadioDeviceLoaderStatusBeginFailed,
+    RadioDeviceLoaderStatusExternalNotFound,
+    RadioDeviceLoaderStatusExternalBeginFailed,
+    RadioDeviceLoaderStatusInternalNotFound,
 } RadioDeviceLoaderStatus;
 
-const SubGhzDevice* radio_device_loader_set(RadioDeviceLoaderStatus* status);
-void radio_device_loader_end(const SubGhzDevice* radio_device);
+const SubGhzDevice* radio_device_loader_set_external(RadioDeviceLoaderStatus* status);
+const SubGhzDevice* radio_device_loader_set_internal(RadioDeviceLoaderStatus* status);
+void radio_device_loader_end_external(const SubGhzDevice* radio_device);
